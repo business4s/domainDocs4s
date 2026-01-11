e88edf469fcf573602476bf5745c0c5c62ef714b
bhometowski <bartek.hometowski@gmail.com>
2026-01-11 20:54:51 +0100

    setup website and initial docs

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,141 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'DomainDocs4S',
+  tagline: 'Generate domain documentation from Scala code',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  url: 'https://business4s.github.io/',
+  baseUrl: '/domaindocs4s/',
+  organizationName: 'business4s',
+  projectName: 'domaindocs4s',
+  trailingSlash: true,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/business4s/domaindocs4s/website',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+          // customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    // image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'DomainDocs4s',
+      logo: {
+        alt: 'DomainDocs4s Logo',
+        src: 'img/domaindocs4s-logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://medium.com/business4s-blog',
+          label: 'Blog',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/business4s/domaindocs4s',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          href: 'https://bit.ly/business4s-discord',
+          label: 'Discord',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     {
+        //       label: 'Docs',
+        //       to: '/docs',
+        //     },
+        //   ],
+        // },
+        {
+          title: 'Community',
+          items: [
+            // {
+            //   label: 'Stack Overflow',
+            //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            // },
+            {
+              label: 'Discord',
+              href: 'https://bit.ly/business4s-discord',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/business4scala',
+            },
+          ],
+        },
+        // {
+        //   title: 'More',
+        //   items: [
+        //     {
+        //       label: 'Blog',
+        //       to: '/blog',
+        //     },
+        //     {
+        //       label: 'GitHub',
+        //       href: 'https://github.com/facebook/docusaurus',
+        //     },
+        //   ],
+        // },
+      ],
+      // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java', 'scala', "json"]
+    },
+    // algolia: {
+    //   appId: '',
+    //   apiKey: '',
+    //   indexName: 'business4s',
+    //   searchPagePath: 'search',
+    // },
+  } satisfies Preset.ThemeConfig,
+  customFields: {
+    domaindocs4sVersion: process.env.DOMAINDOCS4S_VERSION,
+  },
+};
+
+export default config;
