27f8a9118c47bb17dbf03236a446b54950356e64
bhometowski <bartek.hometowski@gmail.com>
2026-01-12 19:07:36 +0100

    fix CI/CD

--- domainDocs4s-examples/src/main/scala/domaindocs4s/order/Domain.scala
+++ domainDocs4s-examples/src/main/scala/domaindocs4s/order/Domain.scala
@@ -1,15 +1,15 @@
 package domaindocs4s.order
 
 object Domain {
-  
+
   // start_domain
   final case class Id(value: Int)
 
-  enum OrderStatus:
+  enum OrderStatus {
     case Draft, Paid, Cancelled
+  }
 
   final case class Order(id: Id, status: OrderStatus)
   // end_domain
-  
-}
 
+}
